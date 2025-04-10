13a57de8189204d7bb4fa7ba40a022652e561464
BIN <55619475+JAVA-BIN@users.noreply.github.com>
2025-04-10 19:33:27 +0900

    project settings

---
+++ src/main/java/io/servlet/inflearnmvcmaster/InflearnMvcMasterApplication.java
@@ -0,0 +1,13 @@
+package io.servlet.inflearnmvcmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnMvcMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnMvcMasterApplication.class, args);
+    }
+
+}
